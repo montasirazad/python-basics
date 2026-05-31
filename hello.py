54ff3157868fdb7e9fb27a67e49106f340595812
Montasir Azad <montasirazad@gmail.com>
2026-06-01 00:25:29 +0600

    python basic practice 01-june-2026

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
-print('hello world')
+print("hello world")
 # my first py code
 # cmnt one
-# cmnt two 
+# cmnt two
 # cmnt three
-print('hello from \n dhrubo')
+# print('hello from \n dhrubo')
+# print('hello from \n dhrubo')
+# print('hello from dhrubo , learning  python')
+if 5 > 2:
+    print("Five is greater than two!")
+if 15 > 2:
+    print("Fifteen is greater than two!")
+print('hello python')
+print(23)
+age=10
+print(age)
